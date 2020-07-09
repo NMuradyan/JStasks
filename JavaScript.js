544304d7b4630ab55df0e7049018c31651e34b5e
NMuradyan <nar.muradian@gmail.com>
2020-07-09 15:00:23 +0400

    JS tasks

---
+++ JavaScript.js
@@ -0,0 +1,22 @@
+let a = 256;
+let b = 121;
+
+let sum = a + b;
+console.log (sum);
+alert (sum);
+
+let dif = a - b;
+console.log (dif);
+alert (dif);
+
+let product = a * b;
+console.log (product);
+alert (product);
+
+let quotient = a / b;
+console.log (quotient);
+alert (quotient);
+
+let remainder = a % b;
+console.log (remainder);
+alert (remainder);
